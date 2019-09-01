97fd043134c1abd9236cbe6566328fa72e58bcab
Rafael Lima <ls.rafael@live.com>
2019-09-01 01:02:55 -0300

    Update 31_desafio_semana_05.js

--- aulas/31_desafio_semana_05.js
+++ aulas/31_desafio_semana_05.js
@@ -69,31 +69,67 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
+function book( nomeLivro ) {
+	var livros = {
+		Programacao: {
+			quantidadePaginas: 210,
+			autor: 'Fulano de Tal',
+			editora: 'Minha Editora'
+		},
+		Redes: {
+			quantidadePaginas: 312,
+			autor: 'Juca Bala',
+			editora: 'Editora Brasileira'
+		},
+		Databases: {
+			quantidadePaginas: 450,
+			autor: 'Foo Bar',
+			editora: 'Editora X'
+		}
+	};
 
+	if ( nomeLivro !== undefined ) {
+		resultado = livros[ nomeLivro ];
+	} else {
+		resultado = livros;
+	}
+
+	return resultado;
+}
 
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-
+console.log( book( ) );
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var nomeLivro = 'Databases';
+var nroPaginas = book( nomeLivro ).quantidadePaginas;
+
+console.log( `O livro ${nomeLivro} tem ${nroPaginas} páginas!`);
+
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+var nomeLivro = 'Redes';
+var nomeAutor = book( nomeLivro ).autor;
+
+console.log( `O autor do livro ${nomeLivro} é ${nomeAutor}.`);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+var nomeLivro = `Programacao`;
+var nomeEditora = book( nomeLivro ).editora;
+
+console.log( `O livro ${nomeLivro} foi publicado pela editora ${nomeEditora}.`)
