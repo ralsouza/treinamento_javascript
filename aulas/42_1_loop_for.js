19ee2b3a5c65718f9dc3bc38ef9d5e335c289799
Rafael Lima <ls.rafael@live.com>
2019-09-10 22:56:41 -0300

    Create 42_1_loop_for.js

---
+++ aulas/42_1_loop_for.js
@@ -0,0 +1,19 @@
+/*
+	Sintaxe:
+	for( init; condition; final-expression)
+*/
+
+// Inicializando uma variável
+for( var num = 0; num <= 20; num++ ) {
+
+	console.log( num );
+
+}
+
+// Inicializando duas variáveis
+for( var num = 2, arroz = 3; num < 5; num++ ) {
+
+	console.log( num );
+
+	console.log( arroz++ );
+}
