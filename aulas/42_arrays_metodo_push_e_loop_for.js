6077a2dbeb7f4c36b69bc85bd13f6ecf99d54c6e
Rafael Lima <ls.rafael@live.com>
2019-09-13 09:39:07 -0300

    Update 42_arrays_metodo_push_e_loop_for.js

--- aulas/42_arrays_metodo_push_e_loop_for.js
+++ aulas/42_arrays_metodo_push_e_loop_for.js
@@ -29,3 +29,25 @@ arr.push( null );
 arr.push( [`a`, `b`, `c` ] );
 
 console.log( arr );
+
+/*
+	Loop For
+	
+	Sintaxe:
+	for( init; condition; final-expression)
+*/
+
+// Inicializando uma variável
+for( var num = 0; num <= 20; num++ ) {
+
+	console.log( num );
+
+}
+
+// Inicializando duas variáveis
+for( var num = 2, arroz = 3; num < 5; num++ ) {
+
+	console.log( num );
+
+	console.log( arroz++ );
+}
