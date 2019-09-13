7489a84880de91466dfe41e4096fa48d8b8bcd31
Rafael Lima <ls.rafael@live.com>
2019-09-13 09:48:18 -0300

    Update 42_arrays_metodo_push_e_loop_for.js

--- aulas/42_arrays_metodo_push_e_loop_for.js
+++ aulas/42_arrays_metodo_push_e_loop_for.js
@@ -1,53 +1,100 @@
-/* 
-	Método push dos arrays, server para adicinar
-	itens no final do array.
+/*
+Crie um array com 5 items (tipos variados).
 */
+var arr = [ 'Rafael', 36, null, true, { peso: 87, profissao: 'ciencia de dados' } ];
 
-var arr = [ 1, 2, 3, `Rafael`, { bola: `azul` }, [ `la1`, `la2`, `la3` ] ];
+/*
+Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
+A função deverá retornar o array atualizado.
+*/
+function addItem( arrayVarName, objToAdd ) {
 
-console.log( arr.length );
+	arrayVarName.push( objToAdd );
 
-// Adicionar um item no array com método push()
-arr.push( { carro: `BMW` } );
+	return arrayVarName;
+}
 
-console.log( arr.length );
-
-// Passar uma função por parâmetro, adicionando uma função no array
-// Uma ideia de programação funcional 
-arr.push( function soma( x, y ) { return x + y; } );
-
-console.log( arr );
-
-// Invocar a função, inserindo dois argumentos para serem somados
-console.log( arr[ 7 ]( 1, 2 ) );
-
-// Adicionando outros valores no array
-// Um valor qualquer
-arr.push( null );
-
-// Outro array
-arr.push( [`a`, `b`, `c` ] );
+/*
+Adicione um novo array ao array criado no início do desafio, com ao menos 3
+itens de tipos diferentes, mostrando o resultado no console.
+*/
+addItem( arr, [ 'Novo Array', function() {}, true ] );
 
 console.log( arr );
 
 /*
-	Loop For
-	
-	Sintaxe:
-	for( init; condition; final-expression)
+Mostre no console o segundo elemento desse último array, criado acima, com a
+frase:
+"O segundo elemento do segundo array é [ELEMENTO]."
 */
+// Primeiro informar o índice do primeiro array, depois o índice do segundo array
+console.log( `O segundo elemento do segundo array é: ` + arr[5][1] );
 
-// Inicializando uma variável
-for( var num = 0; num <= 20; num++ ) {
+/*
+Mostre no console quantos itens tem o primeiro array criado, com a frase:
+"O primeiro array tem [QUANTIDADE DE ITENS] itens."
+*/
+console.log( `O primeiro array tem ` + arr.length + ` itens.` );
 
-	console.log( num );
+/*
+Agora mostre no console quantos itens tem o segundo array criado, com a frase:
+"O segundo array tem [QUANTIDADE DE ITENS] itens."
+*/
+console.log( `O segundo array tem ` + arr[5].length + ` itens.` )
 
+/*
+Utilizando a estrutura de repetição `while`, mostre no console todos os números
+pares entre 10 e 20, inclusive esses 2.
+*/
+console.log( 'Números pares entre 10 e 20:' );
+
+var num = 10;
+
+while( num <= 20 ) {
+
+	if( num % 2 === 0 ) {
+
+		console.log( num );
+	}
+
+	num++;
 }
 
-// Inicializando duas variáveis
-for( var num = 2, arroz = 3; num < 5; num++ ) {
+/*
+Na mesma ideia do exercício acima: mostre agora os números ímpares.
+*/
+console.log( 'Números ímpares entre 10 e 20:' );
+var num = 10;
 
-	console.log( num );
+while( num <= 20 ) {
 
-	console.log( arroz++ );
+	if( num % 2 != 0 ) {
+
+		console.log( num );
+	}
+
+	num++;
+}
+
+/*
+Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
+Só vamos mudar o range:
+- No primeiro "for", mostre os números pares entre 100 e 120, inclusive eles;
+- No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
+*/
+console.log( 'Números pares entre 100 e 120:' );
+for( i = 100; i <= 120; i++ ) {
+
+	if( i % 2 === 0 ) {
+		console.log( i );
+	}
+}
+
+console.log( 'Números ímpares entre 111 e 125:' );
+for( i = 111; i <= 125; i++ ) {
+
+	if( i % 2 != 0 ) {
+
+		console.log( i );
+	}
 }
