3ebc981e22b98ce0426e7b4b65fb8a973630cbf4
Rafael Lima <ls.rafael@live.com>
2019-10-13 14:03:55 -0300

    Create 57_typeof.js

---
+++ aulas/57_typeof.js
@@ -0,0 +1,22 @@
+// How to test the type of values
+
+console.log( typeof undefined );
+
+console.log( typeof function() {} );
+
+console.log( typeof true );
+
+console.log( typeof 10 );
+
+console.log( typeof 'JS Ninja' );
+
+console.log( typeof {} );
+
+console.log( typeof [] );
+
+/*
+	The return as object when test null valueshis 
+	is an error into JavaScript implementation
+*/
+console.log( typeof null );
+
