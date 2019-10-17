86e8bf9d34c57af48b1a023ad9a338f8009796b2
Rafael Lima <ls.rafael@live.com>
2019-10-17 13:29:28 -0300

    Update 57_typeof.js

--- aulas/57_typeof.js
+++ aulas/57_typeof.js
@@ -1,4 +1,8 @@
-// How to test the type of values
+/*
+	Testando tipos de objetos
+
+	Operador unário: typeof <operando>
+*/
 
 console.log( typeof undefined );
 
@@ -8,15 +12,46 @@ console.log( typeof true );
 
 console.log( typeof 10 );
 
-console.log( typeof 'JS Ninja' );
+console.log( typeof NaN );
+
+console.log( typeof 'Rafael' );
+
+// Para todas as outras será um objeto
 
 console.log( typeof {} );
 
 console.log( typeof [] );
 
-/*
-	The return as object when test null valueshis 
-	is an error into JavaScript implementation
-*/
-console.log( typeof null );
+console.log( typeof null ); 
 
+/*
+	O retorno do null como objeto é um erro na implementação
+	do JavaScript. Recomanda-se não usar typeof testando null e objetos
+*/
+
+function subtract(num1, num2) {
+
+	if (typeof num1 === 'number' && typeof num2 === 'number') {
+
+		return num1 - num2;
+	}
+
+	return 'Entre com dois números';
+}
+
+console.log(subtract(4,3))
+
+console.log(subtract(4,'M'))
+
+// Testando o null
+function testeNull(num1, num2) {
+
+	if(num1 === null) {
+		return 'Não entre com valores nulos';
+	}
+
+	return num1 - num2;
+}
+
+console.log( testeNull(9, 3));
+console.log( testeNull(null, 3));
