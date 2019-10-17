2305cc07eab708e9fe4a03c203405c22083817a9
Rafael Lima <ls.rafael@live.com>
2019-10-17 12:48:17 -0300

    Update 56_wrapper_objects.js

--- aulas/56_wrapper_objects.js
+++ aulas/56_wrapper_objects.js
@@ -1,27 +1,58 @@
 /*
-	Wrapper Objects
+	WRAPPER OBJECTS
 
-	Primitive values are not objects, objects has proprieties and methods.
+	Valores primitivos não são objetos, eles são exatamente como são apresentados
+	Objetos possuem propriedades e métodos
 */
 
 var name = 'Rafael';
-
 console.log(name.length);
 
 /*
-	JavaScript has contructors that makes new objects
+	A string acima possui uma propriedade, apensar de ser um valor
+	princípio primitivo. Isso é possível através dos contrutores.
 
-	var name = new String('Rafael');
+	Por baixo dos panos acontece o seguinte:
 
-	var age = new Number(36);
-
-	var ninja = new Boolean(false);
+		var name = new String('Rafael');
+		var age = new Number(30);
+		var ninja = new Boolean(true);
 */
 
+/* 
+	Aqui o valor primitivo é transformado, 
+	onde o JavaScript cria um novo objeto 
+	encapsulando (wrapping up) e finalmente o JS 
+	descarta este objeto da memória
+*/
 var name = new String('Rafael');
 
-// When create a string with new String, the name will always be an object
-console.log(name);
+// Abaixo vemos que a variável foi transformada em um objeto
+console.log(name)
 
-// Return value of name object
-console.log(name.valueOf());
+// Invocar a propriedade length para o objeto name
+console.log(name.length);
+
+// Extrair o valor literal do objeto name
+console.log(name.valueOf())
+
+/*
+	Sem o new os valores são convertidos.
+
+	var name = String(30);
+	var age = Number('36');
+	var ninja = Boolean(1);
+*/
+
+var myVar = String(10);
+
+// O número 10 aqui é uma String
+console.log(myVar);
+
+var myVar = Boolean(0);
+
+console.log(Boolean(0));
+
+console.log(Boolean({}));
+
+console.log(Boolean(!{}));
