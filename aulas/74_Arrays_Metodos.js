e61ed99d2ecbcfd8648f82948f50c6e73fbf5840
Rafael Lima <ls.rafael@live.com>
2019-11-12 09:33:32 -0300

    Update 74_Arrays_Metodos.js

--- aulas/74_Arrays_Metodos.js
+++ aulas/74_Arrays_Metodos.js
@@ -1,46 +1,148 @@
-/*
-	.join()
+;(function() {
 
-	Juntar itens de um array
-*/
-var arr = ['arroz','feijao','lasanha'];
+	/*
+		Envolva todo o conteúdo desse arquivo em uma IIFE.
+	*/
 
-// Juntar em uma string os itens do array,
-// com separador de vírgula, por padrão
-var arrJoin = arr.join();
+	/*
+		Crie um objeto chamado `person`, com as propriedades:
+		    `name`: String
+		    `lastname`: String
+		    `age`: Number
+		Preencha cada propriedade com os seus dados pessoais, respeitando o tipo
+		de valor para cada propriedade.
+	*/
+	
+	var person = {
+		name: 'Rafael',
+		lastname: 'Lima',
+		age: 36
+	};
 
-console.log(arrJoin);
 
-// Usar outro tipo de separador separador em branco
-console.log( arr.join(' ') );
+	console.log( 'Propriedades de "person":' );
 
-// espaço, pipe e espaço
-console.log( arr.join(' | ') );
+	/*
+		Mostre no console, em um array, todas as propriedades do objeto acima.
+		Não use nenhuma estrutura de repetição, nem crie o array manualmente.
+	*/
+	console.log( Object.keys( person ) );
 
-// Criando frases
-console.log( 'Meu almoço será ', arr.join(', ') );
+	/*
+		Crie um array vazio chamado `books`.
+	*/
+	var books = [];
 
-/*
-	.reverse()
+	/*
+		Adicione nesse array 3 objetos, que serão 3 livros. Cada livro deve ter a
+		seguintes propriedades:
+		`name`: String
+		`pages`: Number
+	*/
+	books.push( 
+		{ name: 'Livro 1',
+		  pages: 300
+		},
+		{ name: 'Livro 2',
+		  pages: 500
+		},
+		{ name: 'Livro 3',
+		  pages: 250
+		}
+	);
 
-	Inverter a ordem dos itens do array
-	ESTE MÉTODO MODIFICA O ARRAY
-*/
-console.log( arr );
+	console.log( '\nLista de livros:' );
 
-console.log( arr.reverse() );
+	/*
+		Mostre no console todos os livros.
+	*/
+	console.log( books );
 
-// O array é modificado
-console.log( arr );
+	console.log( '\nLivro que está sendo removido:' );
+	/*
+		Remova o último livro, e mostre-o no console.
+	*/
+	console.log( books.pop() );
 
-/*
-	.sort()
+	// Ou também, atribuindo o valor removido em uma variável
+	// lastValue = books.pop();
 
-	Ordenar os itens do array
-	ESTE MÉTODO MODIFICA O ARRAY
-*/
-var arr2 = ['omega', 'alpha', 'zeta', 'beta'];
+	console.log( '\nAgora sobraram somente os livros:' );
+	/*
+		Mostre no console os livros restantes.
+	*/
+	console.log( books );
 
-console.log( arr2.sort() );
+	/*
+		Converta os objetos que ficaram em `books` para strings.
+	*/
+	var books = JSON.stringify( books );
 
-console.log( arr2 );
+	console.log( '\nLivros em formato string:' );
+
+	/*
+		Mostre os livros nesse formato no console:
+	*/
+	console.log( books );
+
+	/*
+		Converta os livros novamente para objeto.
+	*/
+	var books = JSON.parse( books );
+
+	console.log( '\nAgora os livros são objetos novamente:' );
+	console.log( books );
+
+	/*
+		Mostre no console todas as propriedades e valores de todos os livros,
+		no formato abaixo:
+		    "[PROPRIEDADE]: [VALOR]"
+	*/
+	
+	console.log('\nAs propriedades no formato "[PROPRIEDADE]: [VALOR]": (Forma manual)');
+
+	for( var i = 0; i < books.length; i++) {
+
+		console.log( books[i].name + ': ' + books[i].pages );
+	}
+
+
+	console.log('\nAs propriedades no formato "[PROPRIEDADE]: [VALOR]": (Forma dinâmica)');
+
+	for( var i = 0; i < books.length; i++ ) {
+
+		for( prop in books[i] ) {
+
+			console.log( prop + ': ' + books[i][prop] );
+		}
+	}
+
+	/*
+		Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
+		seu nome. Adicione seu nome completo no array.
+	*/
+	var myName = ['R','a','f','a','e','l',' ','L','i','m','a'];
+
+	console.log( '\nMeu nome é:' );
+
+	/*
+		Juntando todos os itens do array, mostre no console seu nome.
+	*/
+	console.log( myName.join('') );
+
+	console.log( '\nMeu nome invertido é:' );
+ 
+	/*
+		Ainda usando o objeto acima, mostre no console seu nome invertido.
+	*/
+	console.log( myName.reverse().join('') )
+
+	console.log( '\nAgora em ordem alfabética:' );
+	/*
+		Mostre todos os itens do array acima, odenados alfabéticamente.
+	*/
+	console.log( myName.sort() );
+
+	// O padrão do JS é retornar primeiro os espaços, depois as letras maiusculas e por fim as minúsculas
+
+})();
