5df689b3094d161f5839c5fee4811d7ffef46d03
Rafael Lima <ls.rafael@live.com>
2020-03-24 11:53:06 -0300

    Update main.js

--- aulas/117_desafio_19/js/main.js
+++ aulas/117_desafio_19/js/main.js
@@ -111,6 +111,22 @@
 	*/
 	var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
 	console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
-	// ?
+	
+	function hasClass( markup, cssClass ) {
+
+		//var regex = new RegExp( 'class=[\'"](?:[\\w\\s]+)?'+ cssClass +'(?:[\\w\\s]+)?[\'"]' );
+		
+		// Or more easy
+		var regex = new RegExp( 'class=[\'"].*' + cssClass + '.*[\'"]' );
+
+		return regex.test( markup );
+	}
+
+	var classes = ['container', 'text', 'date', 'excerpt', 'main'];
+
+	classes.forEach( function( cssClass ) {
+
+		console.log( hasClass( markup, cssClass ) + ' para a classe ' + cssClass );
+	})
 
 })();
